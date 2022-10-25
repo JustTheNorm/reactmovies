52a68a3b673ed0ff759ede650b72bc0ab36a6498
Norman <whosnorm@gmail.com>
2022-10-25 13:08:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
 
 function App() {
+// state to hold the movie data
+const [movie, setMovie] = useState(null)
+
+// Function to fetch movie data
+const getMovie = async (searchTerm) => {
+  const res = fetch (`https://www.omdbapi.com/?apikey=658ac9d6&t=${searchTerm}`)
+  const data = await res.json()
+  console.log(data)
+  setMovie(data)
+  
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Movies</h1>
+      <Form movieSearch={getMovie}/>
+      <MovieDisplay />
     </div>
   );
 }
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,13 @@
+import react from "react"
+
+const MovieDisplay = () => {
+    return (
+        <div>
+            <h1>Display Movie</h1>
+        
+        
+        </div>
+    )
+}
+
+export default MovieDisplay
